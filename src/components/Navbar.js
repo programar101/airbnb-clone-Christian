b7977f4e2f71e0d83dcd91650c992004c3c1e768
Christian Carballo <carballo.chr@gmail.com>
2022-05-08 02:06:20 -0300

    Imagen-problema

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,6 @@ import airbnblogo from "../components/images/airbnblogo.png";
 export default function Navbar() {
     return (
         <nav>
-
             <image src={airbnblogo} className="nav--logo" alt=""/>
             <h2>Hola</h2>
         </nav>
