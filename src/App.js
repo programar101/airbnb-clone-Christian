32c015ed754b9cc08fb6d15a8e4df321b61ba591
Christian Carballo <carballo.chr@gmail.com>
2022-05-18 23:34:09 -0300

    data.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 
 
 export default function App() {
+
+    const cards = data.map(item => {
+        return (
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })     
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card 
-                img="../images/image1.jpg"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life Lessons with Katie Zaferes"
-                price={136}
-            />
+            {cards}
+            
         </div>
     )}
