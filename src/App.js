a0f47e41b985f6660c4be5f268300bbf1dd07a0f
Christian Carballo <carballo.chr@gmail.com>
2022-05-18 23:06:08 -0300

    Carga de Imagenes

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
-import image1 from "./components/images/image1.jpg"; 
+
 
 export default function App() {
-    return ( 
+    return (
         <div>
             <Navbar />
             <Hero />
             <Card 
-                img={image1}
+                img="../images/image1.jpg"
                 rating="5.0"
                 reviewCount={6}
                 country="USA"
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,10 @@
 import React from "react"
-import grid from "../components/images/grid.png";
+
 
 export default function Hero() {
     return (
         <section className="hero">
-            <img src={grid} className="hero--photo" alt=""/>
+            <img src="../images/grid.png" className="hero--photo" alt=""/>
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
